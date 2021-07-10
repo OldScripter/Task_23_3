934e52abb8d504ddd5976b4ad88e7087b2b418f6
OldScripter <Oldscripter@gmail.com>
2021-07-10 14:27:22 +0300

    feature (main) add macroses

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+#define WINTER
+
+
+#if defined SUMMER
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "SUMMER" << std::endl;
     return 0;
 }
+#endif
+
+#if defined WINTER
+int main() {
+    std::cout << "WINTER" << std::endl;
+    return 0;
+}
+#endif
+
+#if defined AUTUMN
+int main() {
+    std::cout << "AUTUMN" << std::endl;
+    return 0;
+}
+#endif
+
+#if defined SPRING
+int main() {
+    std::cout << "SPRING" << std::endl;
+    return 0;
+}
+#endif
+
+
+
+
+
+
+
+
